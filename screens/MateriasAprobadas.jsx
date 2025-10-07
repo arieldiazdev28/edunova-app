71d2e35469cc4d1ba25124aeb33239467639400b
arieldiazdev28 <arieldiaz.inac10@gmail.com>
2025-10-07 00:26:52 -0600

    Creación del menú lateral para la navegación dentro de la aplicación

---
+++ screens/MateriasAprobadas.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MateriasAprobadas = ({ navigation }) => {
+  return (
+    <View>
+      <Text>MateriasAprobadas</Text>
+    </View>
+  );
+};
+
+export default MateriasAprobadas;
---
+++ screens/MateriasInscritas.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MateriasInscritas = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Materias Inscritas</Text>
+    </View>
+  );
+};
+
+export default MateriasInscritas;
